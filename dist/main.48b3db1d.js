9815624ca604ad26bd5ee1fe54065429b1abd763
AlexanderBelfort <alexanderphoenixbusiness@mail.bg>
2020-09-16 15:59:37 +0100

    aleats mini finished

--- dist/main.48b3db1d.js
+++ dist/main.48b3db1d.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "1941" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "4616" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
